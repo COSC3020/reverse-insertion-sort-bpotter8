021471b133b1b211ee2ada5104133bf3841208be
Braden Potter <144744976+bpotter8@users.noreply.github.com>
2024-02-16 16:33:59 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,11 @@
 function insertionSortReverse(array) {
+    for(var i = array.length; i >= 0; i--){
+        var val = array[i];
+        var j;
+        for(j = i; j > 0 && arr[j-1] > val; j--){
+            arr[j] = arr[j - 1];
+        }
+        arr[j] = val;
+    }
     return array;
 }
